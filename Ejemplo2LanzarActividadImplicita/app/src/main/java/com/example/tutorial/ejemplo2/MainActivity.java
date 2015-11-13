f227929f72f92cb5c92e77510db9c09a2f616186
alfespa17 <alfespa17@gmail.com>
2015-11-12 22:19:55 -0600

    Ejemplo2

---
+++ Ejemplo2LanzarActividadImplicita/app/src/main/java/com/example/tutorial/ejemplo2/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.tutorial.ejemplo2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //Creamos una variable para poder tener una referencia al boton de lanzar
+    public Button mLanzarBoton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Utilizamos la funcion findViewById para obtener la referncia al boton y hacemos un cast al tipo Button
+        mLanzarBoton=(Button)findViewById(R.id.lanzarDos);
+
+
+        //Agregamos un accion a ejecutarse al dar click al boton
+        mLanzarBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Inicializamos un objeto de tipo Intent con la referencia a la actividad que necesitamos lanzar
+                Intent intent=new Intent();
+                intent.setClass(getApplicationContext(),Dos.class);
+
+                //Lanzamos la actividad
+                startActivity(intent);
+
+            }
+        });
+    }
+}
